81455f9d843d5f41f50a0c39f5a99ab771582797
Aditya Shandilya <85393000+AdityaShandilya23@users.noreply.github.com>
2023-12-10 15:32:09 +0530

    Update socket_send.py

--- socket_send.py
+++ socket_send.py
@@ -5,80 +5,77 @@ PACKET_SIZE = 1024
 
 
 class Client:
-	def __init__(self,ip,port):
-		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.socket.connect((ip,port))
-		print("Connected...")
-		self.start()
-	def send_msg(self,msg):
-		message = str(msg).encode()
-		msg_length = len(message)
-		send_length = str(msg_length).encode()
-		send_length += b' ' * (PACKET_SIZE - len(send_length))
-		self.socket.send(send_length)
-		self.socket.send(message) 
-		
+    def __init__(self, ip, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((ip, port))
+        print("Connected...")
+        self.start()
 
-	def recv_msg(self):
-		msg_length = self.socket.recv(1024)
-		data = self.socket.recv(int(msg_length))
-		return data
-	
-	def start(self):
-		pass
-		#logic or override this function 
+    def send_msg(self, msg):
+        message = str(msg).encode()
+        msg_length = len(message)
+        send_length = str(msg_length).encode()
+        send_length += b' ' * (PACKET_SIZE - len(send_length))
+        self.socket.send(send_length)
+        self.socket.send(message)
+
+    def recv_msg(self):
+        msg_length = self.socket.recv(1024).strip()
+        if msg_length:
+            data = self.socket.recv(int(msg_length))
+            return data
+        return b''
+
+    def start(self):
+        pass
+        # logic or override this function
 
 
+class Server:
+    def __init__(self, ip, port):
+        self.address = (ip, port)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((ip, port))
+        self.server_socket.listen()
+        print("Started server at:", self.address)
+        self.valid_ips = []
+        self.client_list = {}
 
+        while True:
+            self.run()
 
-class Sever:
-	def __init__(self,ip,port):
-		self.address = [ip,port]
-		self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		self.server_socket.bind((ip,port))
-		self.server_socket.listen()
-		print("Started server at :",self.adress)
-		self.valid_ips = []
-		self.client_list = {}
-		
-		while True:
-			self.run()
+    def accept_connections(self, connection, address):
+        self.client_list[address] = connection
+        self.talk(address)
 
-	def accept_connections(self,connection,address):
-		self.client_list[address] = connection
-		self.talk(adress)
-		
-		
-	def talk(self,address):
-		if address in valid_ips:
-			pass
-		   #logic or override this function 
-		
-	def add_valid_ip(self,address):
-		self.valid_ips.append(str(adress))
+    def talk(self, address):
+        pass
+        # logic or override this function
 
-	def send_msg(self,msg,conn):
-		message = str(msg).encode()
-		msg_length = len(message)
-		send_length = str(msg_length).encode()
-		send_length += b' ' * (PACKET_SIZE - len(send_length))
-		conn.send(send_length)
-		conn.send(message)    
+    def add_valid_ip(self, address):
+        self.valid_ips.append(str(address))
 
-	
-	def recieve(self,conn):
-		msg_length = conn.recv(1024)
-		data = conn1.recv(int(msg_length))
-		return data
+    def send_msg(self, msg, conn):
+        message = str(msg).encode()
+        msg_length = len(message)
+        send_length = str(msg_length).encode()
+        send_length += b' ' * (PACKET_SIZE - len(send_length))
+        conn.send(send_length)
+        conn.send(message)
 
-		
-	
-	def run(self):
-		(clientConnected, clientAddress) = self.server_socket.accept()
-		thread  = threading.Thread(target = self.accept_connections , args = (clientConnected,clientAddress))
-		thread.start()
-		
+    def receive(self, conn):
+        msg_length = conn.recv(1024).strip()
+        if msg_length:
+            data = conn.recv(int(msg_length))
+            return data
+        return b''
+
+    def run(self):
+        client_connected, client_address = self.server_socket.accept()
+        thread = threading.Thread(target=self.accept_connections, args=(client_connected, client_address))
+        thread.start()
 
 
 if __name__ == "__main__":
-	pass
+    
+    pass
